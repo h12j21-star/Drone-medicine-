7d1b56113ede843d10b09db30427cb005ed28756
h12j21-star <jh06079@naver.com>
2023-03-29 19:23:32 +0900

    장바구니페이지

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import ShoppingBasket from './components/shoppingBasket/ShoppingBasket';
 import { GlobalStyle } from './style/GlobalStyle';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import Basket from './pages/Basket';
+
 function App() {
     return (
         <>
             <Provider store={store}>
                 <GlobalStyle />
                 <div className="App">
-                    <ShoppingBasket />
+                    <Basket />
                 </div>
             </Provider>
         </>
