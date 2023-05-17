b70b9b8cb449d028c51913d5a217022aeb8968e7
chanhoim <cksgh201@naver.com>
2023-05-17 13:53:52 +0900

    initial setting  product-service & eureka-server

---
+++ backend/eureka-server/src/test/java/dronemedicine/backend/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dronemedicine.backend.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
